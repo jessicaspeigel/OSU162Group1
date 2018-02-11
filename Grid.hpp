6fcef79093f36320659935547a7db86c57531d49
Ryan <ryanjmack21@gmail.com>
2018-02-11 13:24:13 -0500

    Improve readability

--- Grid.hpp
+++ Grid.hpp
@@ -10,13 +10,11 @@
 #include "Critter.hpp"
 #include "Ant.hpp"
 #include "Doodlebug.hpp"
-
 #include <string>
 using std::string;
 
 class Grid
 {
-
 enum cells {SPACE, ANT, DOODLEBUG}; 
 
 private:
@@ -25,37 +23,47 @@ private:
 	int doodleBugs;
 	int rows;
 	int columns;
-    int* surroundingCells;
-    Critter*** grid;	
+    	int* surroundingCells;
+    	Critter*** grid;	
 
 public:
 	Grid();
+	
+	
     /****************************************************************************
     ** Randomly place ants and doodlebugs in grid
     ****************************************************************************/
     void fillGrid(); 
+	
+	
     /****************************************************************************
     ** Loops through Grid::grid and calls move function for each critter, mutates
-        grid as necessary
+    ** grid as necessary
     ****************************************************************************/
     void moveCritters();
+	
+	
 	/****************************************************************************
     ** Prints the grid
     ****************************************************************************/
 	void printGrid();
+	
+	
     /****************************************************************************
     ** Input: int row, int col. If grid[row][col] == nullptr, else returns 
-        the critter's type.
+    ** the critter's type.
     ****************************************************************************/
     int checkCell(int, int); 
     void clearCell(int, int); 
+	
+	
     /****************************************************************************
     ** Input: int row, int col (the location of a critter in the grid). For each 
-        critter position, this function mutates the Grid::surroundingCells array. 
-        surroundingCells[0] = cell above critter
-        surroundingCells[1] = cell right of critter
-        surroundingCells[2] = cell below critter
-        surroundingCells[3] = cell left of critter
+    ** critter position, this function mutates the Grid::surroundingCells array. 
+    ** surroundingCells[0] = cell above critter
+    ** surroundingCells[1] = cell right of critter
+    ** surroundingCells[2] = cell below critter
+    ** surroundingCells[3] = cell left of critter
     ****************************************************************************/
     void getSurroundingCells(int, int); 
     int getSteps(); 
