ee6129d14b54ce5c437dc3832fb7000c567748be
Ryan <ryanjmack21@gmail.com>
2018-02-11 13:20:53 -0500

    Improve readability

--- Doodlebug.hpp
+++ Doodlebug.hpp
@@ -1,28 +1,31 @@
 /**********************************************************************
-** Program
+** Group Project 1 Program name: Predator-Prey Game
 ** Author: Group 1
-** Date : 2/ / 18
-* * Description :
-
+** Date: 2/ / 18
+** Description: 
 *********************************************************************/
 
-#pragma once
+#ifndef DOODLEBUG_HPP
+#define DOODLEBUG_HPP
+
 #include "Critter.hpp"
 
 class Doodlebug : public Critter
 {
 private:
-    int timeSinceFeed; //track time since feeding
-    bool starveFlag; //is the doodlebug starved?
+    int timeSinceFeed; 	 	//track time since feeding
+    bool starveFlag; 		//is the doodlebug starved?
     int daysToStarvation;
+
 public:
-	Doodlebug(int, int);
+    Doodlebug(int, int);
+    
     /****************************************************************************
     ** Input: pointer to an int array created in the Grid class. Array represents
-        cells adjacent to doodlebug. If ants in adjacent cells, doodlebug eats, 
-        else it tries to move to random cell. If move is valid, doodlebug
-        adjusts row and column and returns true. Function returns false otherwise, 
-        and no move made
+    ** cells adjacent to doodlebug. If ants in adjacent cells, doodlebug eats, 
+    ** else it tries to move to random cell. If move is valid, doodlebug
+    ** adjusts row and column and returns true. Function returns false otherwise, 
+    ** and no move made
     ****************************************************************************/
     virtual bool move(int*); 
     virtual int getType();
@@ -32,3 +35,4 @@ public:
 
 };
 
+#endif
